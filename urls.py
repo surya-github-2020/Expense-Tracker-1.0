fd6d1f5b2d55bc3bcaa2538e87d2628ff7f88aad
surya-github-2020 <74919789+surya-github-2020@users.noreply.github.com>
2022-02-24 00:26:40 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,16 +1,25 @@
-from .views import RegistrationView, UsernameValidationView, EmailValidationView, LogoutView, VerificationView, LoginView
-from django.urls import path
-from django.views.decorators.csrf import csrf_exempt
+"""expenseswebsite URL Configuration
 
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('register', RegistrationView.as_view(), name="register"),
-    path('login', LoginView.as_view(), name="login"),
-    path('logout', LogoutView.as_view(), name="logout"),
-    path('validate-username', csrf_exempt(UsernameValidationView.as_view()),
-         name="validate-username"),
-    path('validate-email', csrf_exempt(EmailValidationView.as_view()),
-         name='validate_email'),
-    path('activate/<uidb64>/<token>',
-         VerificationView.as_view(), name='activate'),
+    path('', include('expenses.urls')),
+    path('authentication/', include('authentication.urls')),
+    path('preferences/', include('userpreferences.urls')),
+    path('income/', include('userincome.urls')),
+    path('admin/', admin.site.urls),
 ]
--- views.py
+++ views.py
@@ -1,158 +1,144 @@
 from django.shortcuts import render, redirect
-from django.views import View
-import json
-from django.http import JsonResponse
-from django.contrib.auth.models import User
-import json
-from django.http import JsonResponse
-from django.contrib.auth.models import User
-from validate_email import validate_email
-from django.contrib import messages
-from django.core.mail import EmailMessage
-from django.contrib.sites.shortcuts import get_current_site
-from django.utils.encoding import force_bytes, force_text, DjangoUnicodeDecodeError
-from django.core.mail import send_mail
-from django.contrib.sites.shortcuts import get_current_site
-from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
-from django.template.loader import render_to_string
-from .utils import account_activation_token
-from django.urls import reverse
-from django.contrib import auth
-
+from django.contrib.auth.decorators import login_required
+from .models import Category, Expense
 # Create your views here.
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.core.paginator import Paginator
+import json
+from django.http import JsonResponse
+from userpreferences.models import UserPreference
+import datetime
 
 
-class EmailValidationView(View):
-    def post(self, request):
-        data = json.loads(request.body)
-        email = data['email']
-        if not validate_email(email):
-            return JsonResponse({'email_error': 'Email is invalid'}, status=400)
-        if User.objects.filter(email=email).exists():
-            return JsonResponse({'email_error': 'sorry email in use,choose another one '}, status=409)
-        return JsonResponse({'email_valid': True})
+def search_expenses(request):
+    if request.method == 'POST':
+        search_str = json.loads(request.body).get('searchText')
+        expenses = Expense.objects.filter(
+            amount__istartswith=search_str, owner=request.user) | Expense.objects.filter(
+            date__istartswith=search_str, owner=request.user) | Expense.objects.filter(
+            description__icontains=search_str, owner=request.user) | Expense.objects.filter(
+            category__icontains=search_str, owner=request.user)
+        data = expenses.values()
+        return JsonResponse(list(data), safe=False)
 
 
-class UsernameValidationView(View):
-    def post(self, request):
-        data = json.loads(request.body)
-        username = data['username']
-        if not str(username).isalnum():
-            return JsonResponse({'username_error': 'username should only contain alphanumeric characters'}, status=400)
-        if User.objects.filter(username=username).exists():
-            return JsonResponse({'username_error': 'sorry username in use,choose another one '}, status=409)
-        return JsonResponse({'username_valid': True})
+@login_required(login_url='/authentication/login')
+def index(request):
+    categories = Category.objects.all()
+    expenses = Expense.objects.filter(owner=request.user)
+    paginator = Paginator(expenses, 5)
+    page_number = request.GET.get('page')
+    page_obj = Paginator.get_page(paginator, page_number)
+    currency = UserPreference.objects.get(user=request.user).currency
+    context = {
+        'expenses': expenses,
+        'page_obj': page_obj,
+        'currency': currency
+    }
+    return render(request, 'expenses/index.html', context)
 
 
-class RegistrationView(View):
-    def get(self, request):
-        return render(request, 'authentication/register.html')
+@login_required(login_url='/authentication/login')
+def add_expense(request):
+    categories = Category.objects.all()
+    context = {
+        'categories': categories,
+        'values': request.POST
+    }
+    if request.method == 'GET':
+        return render(request, 'expenses/add_expense.html', context)
 
-    def post(self, request):
-        # GET USER DATA
-        # VALIDATE
-        # create a user account
+    if request.method == 'POST':
+        amount = request.POST['amount']
 
-        username = request.POST['username']
-        email = request.POST['email']
-        password = request.POST['password']
+        if not amount:
+            messages.error(request, 'Amount is required')
+            return render(request, 'expenses/add_expense.html', context)
+        description = request.POST['description']
+        date = request.POST['expense_date']
+        category = request.POST['category']
 
-        context = {
-            'fieldValues': request.POST
-        }
+        if not description:
+            messages.error(request, 'description is required')
+            return render(request, 'expenses/add_expense.html', context)
 
-        if not User.objects.filter(username=username).exists():
-            if not User.objects.filter(email=email).exists():
-                if len(password) < 6:
-                    messages.error(request, 'Password too short')
-                    return render(request, 'authentication/register.html', context)
+        Expense.objects.create(owner=request.user, amount=amount, date=date,
+                               category=category, description=description)
+        messages.success(request, 'Expense saved successfully')
 
-                user = User.objects.create_user(username=username, email=email)
-                user.set_password(password)
-                user.is_active = False
-                user.save()
-                current_site = get_current_site(request)
-                email_body = {
-                    'user': user,
-                    'domain': current_site.domain,
-                    'uid': urlsafe_base64_encode(force_bytes(user.pk)),
-                    'token': account_activation_token.make_token(user),
-                }
-
-                link = reverse('activate', kwargs={
-                               'uidb64': email_body['uid'], 'token': email_body['token']})
-
-                email_subject = 'Activate your account'
-
-                activate_url = 'http://'+current_site.domain+link
-
-                email = EmailMessage(
-                    email_subject,
-                    'Hi '+user.username + ', Please the link below to activate your account \n'+activate_url,
-                    'noreply@semycolon.com',
-                    [email],
-                )
-                email.send(fail_silently=False)
-                messages.success(request, 'Account successfully created')
-                return render(request, 'authentication/register.html')
-
-        return render(request, 'authentication/register.html')
+        return redirect('expenses')
 
 
-class VerificationView(View):
-    def get(self, request, uidb64, token):
-        try:
-            id = force_text(urlsafe_base64_decode(uidb64))
-            user = User.objects.get(pk=id)
+@login_required(login_url='/authentication/login')
+def expense_edit(request, id):
+    expense = Expense.objects.get(pk=id)
+    categories = Category.objects.all()
+    context = {
+        'expense': expense,
+        'values': expense,
+        'categories': categories
+    }
+    if request.method == 'GET':
+        return render(request, 'expenses/edit-expense.html', context)
+    if request.method == 'POST':
+        amount = request.POST['amount']
 
-            if not account_activation_token.check_token(user, token):
-                return redirect('login'+'?message='+'User already activated')
+        if not amount:
+            messages.error(request, 'Amount is required')
+            return render(request, 'expenses/edit-expense.html', context)
+        description = request.POST['description']
+        date = request.POST['expense_date']
+        category = request.POST['category']
 
-            if user.is_active:
-                return redirect('login')
-            user.is_active = True
-            user.save()
+        if not description:
+            messages.error(request, 'description is required')
+            return render(request, 'expenses/edit-expense.html', context)
 
-            messages.success(request, 'Account activated successfully')
-            return redirect('login')
+        expense.owner = request.user
+        expense.amount = amount
+        expense. date = date
+        expense.category = category
+        expense.description = description
 
-        except Exception as ex:
-            pass
+        expense.save()
+        messages.success(request, 'Expense updated  successfully')
 
-        return redirect('login')
-
-
-class LoginView(View):
-    def get(self, request):
-        return render(request, 'authentication/login.html')
-
-    def post(self, request):
-        username = request.POST['username']
-        password = request.POST['password']
-
-        if username and password:
-            user = auth.authenticate(username=username, password=password)
-
-            if user:
-                if user.is_active:
-                    auth.login(request, user)
-                    messages.success(request, 'Welcome, ' +
-                                     user.username+' you are now logged in')
-                    return redirect('expenses')
-                messages.error(
-                    request, 'Account is not active,please check your email')
-                return render(request, 'authentication/login.html')
-            messages.error(
-                request, 'Invalid credentials,try again')
-            return render(request, 'authentication/login.html')
-
-        messages.error(
-            request, 'Please fill all fields')
-        return render(request, 'authentication/login.html')
+        return redirect('expenses')
 
 
-class LogoutView(View):
-    def post(self, request):
-        auth.logout(request)
-        messages.success(request, 'You have been logged out')
-        return redirect('login')
+def delete_expense(request, id):
+    expense = Expense.objects.get(pk=id)
+    expense.delete()
+    messages.success(request, 'Expense removed')
+    return redirect('expenses')
+
+
+def expense_category_summary(request):
+    todays_date = datetime.date.today()
+    six_months_ago = todays_date-datetime.timedelta(days=30*6)
+    expenses = Expense.objects.filter(owner=request.user,
+                                      date__gte=six_months_ago, date__lte=todays_date)
+    finalrep = {}
+
+    def get_category(expense):
+        return expense.category
+    category_list = list(set(map(get_category, expenses)))
+
+    def get_expense_category_amount(category):
+        amount = 0
+        filtered_by_category = expenses.filter(category=category)
+
+        for item in filtered_by_category:
+            amount += item.amount
+        return amount
+
+    for x in expenses:
+        for y in category_list:
+            finalrep[y] = get_expense_category_amount(y)
+
+    return JsonResponse({'expense_category_data': finalrep}, safe=False)
+
+
+def stats_view(request):
+    return render(request, 'expenses/stats.html')
